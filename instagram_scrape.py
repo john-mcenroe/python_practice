b88f4c60eba06427d37111e1eceb03cc3b430511
John William McEnroe <johnwilliammcenroe@Rachelles-MacBook-Air.local>
2023-04-01 10:11:05 -0700

    instag commit

---
+++ instagram_scrape.py
@@ -0,0 +1,7 @@
+from instascrape import *
+
+# Instantiate the scraper objects 
+profile = Profile('chris_greening')
+
+# Count the number of hastags google posted in the last month
+profile.scrape()
